98e763e1a8cabd3d1b4d6a83921a06f95bb8dfb3
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-09-28 19:03:04 +0500

    Fix eslint error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 import './Calculator.css';
 
-
 const Calculator = () => {
   const [state, setstate] = useState({
     total: 0,
